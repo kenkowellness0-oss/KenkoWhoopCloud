3e9e9ddd143ffe39ccf8522b8e8735653bb9abeb
kenkowellness0-oss <kenkowellness0@gmail.com>
2025-12-28 13:00:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,14 @@ import os
 import requests
 import json
 
-print("WHATSAPP_URL:", repr(WHATSAPP_URL))
+# Get secrets FIRST
+WHATSAPP_URL = os.getenv("WHATSAPP_URL")
+WHATSAPP_API_KEY = os.getenv("WHATSAPP_API_KEY")
+PHONE_NUMBER = os.getenv("PHONE_NUMBER")
+
+# NOW debug works
+print("WHATSAPP_URL:", repr(WHATSAPP_URL))      # ← Add this
+print("WHATSAPP_API_KEY:", repr(WHATSAPP_API_KEY[:10]) + "...")  # First 10 chars
+print("PHONE_NUMBER:", repr(PHONE_NUMBER))
+
+# Rest of your code...
